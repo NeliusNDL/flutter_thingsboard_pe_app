6aa4a95f0aa5cc2dd075cd02c4a01ea2130572ba
theo-hubble <theo.w@hubblelithium.co.za>
2024-10-03 10:15:59 +0200

    .

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,7 +1,7 @@
 abstract class ThingsboardAppConstants {
   static final thingsBoardApiEndpoint = 'https://portal.riotsystems.cloud';
   static final thingsboardOAuth2CallbackUrlScheme =
-      'com.hubblelithium.app.auth';
+      'com.hubbleenergy.app.auth';
 
   /// Not for production (only for debugging)
   // When building for iOS use nDoC7obkQqhZAUJBgASAStW1 and Android use XCycH79WyGLo26VI6G9tPxux
